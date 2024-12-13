fd1b6255dc3556aa6169ed066da02796912822f7
Mouwneam <124057919+Mouwneam@users.noreply.github.com>
2024-12-13 09:46:46 +0700

    update service card

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -2,7 +2,7 @@
 const ServicesCard = (props) => {
   return (
     <div className="w-[500px] sm:w-[350px] md:w-[230px] lg:w-[300px] sm:min-w-[200px] flex 
-    flex-col justify-center items-center px-5 gap-4 ">
+    flex-col justify-center items-center px-5 gap-4 max-sm:w-full">
       <div>
         <img 
             src={props.icon} 
@@ -10,8 +10,8 @@ const ServicesCard = (props) => {
             className="w-[75px] h-[75px]"    
         />
     </div>
-      <p className="text-2xl font-palanquin text-center">{props.title}</p>
-      <p className="text-center">{props.subtext}</p>
+      <p className="text-2xl max-sm:text-xl font-palanquin text-center">{props.title}</p>
+      <p className="text-center max-sm:text-sm">{props.subtext}</p>
     </div>
   )
 }
